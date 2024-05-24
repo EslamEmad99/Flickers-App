fa37d7180c3ba8d423d7f7492af72ca4d64684ae
eslam <eslamemad903@gmail.com>
2024-05-25 01:57:34 +0300

    - Fragment movies list design

---
+++ app/src/main/java/com/eslam/flickers_app/ui/main/movies/MoviesFragment.kt
@@ -0,0 +1,10 @@
+package com.eslam.flickers_app.ui.main.movies
+
+import com.eslam.flickers_app.databinding.FragmentMoviesBinding
+import com.eslam.flickers_app.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MoviesFragment : BaseFragment<FragmentMoviesBinding>(FragmentMoviesBinding::inflate) {
+
+}
--- app/src/main/java/com/eslam/flickers_app/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/eslam/flickers_app/ui/splash/SplashActivity.kt
@@ -1,13 +1,16 @@
 package com.eslam.flickers_app.ui.splash
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.eslam.flickers_app.ui.main.MainActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
